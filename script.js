8ff43f3c63be7f3cc2c58cb8a0f4254fa9506d4d
Sophia Ferrera <slf445@nyu.edu>
2020-11-15 15:16:22 -0500

    new format

--- script.js
+++ script.js
@@ -1,62 +1,64 @@
-let outputElement = document.getElementById('outputElement');
-let outputParagraph = document.getElementById('outputParagraph');
+let forgettingGrid = document.getElementById('grid1');
+let learningGrid = document.getElementById('grid2');
+let sadGrid = document.getElementById('grid3');
+
 
 let jsonDatabase = [{
-    "title": "The Lobster",
-    "description": ["In a dystopian society, single people must find a mate within 45 days or be transformed into an animal of their choice."];
+    "genre": "forgetting",
+    "title": "Easy A",
+    "picture": "https://img.timeinc.net/time/daily/2010/1009/360_easy_a_0917.jpg",
+    "description": "When a lie about Olive's reputation transforms her from anonymous to infamous at her high school, she decides to embrace a provocative new persona.",
+    "link": "https://www.netflix.com/search?q=easy%20a&jbv=70123920"
   },
+
   {
-    "title": "Her",
-    "description": ["Left heartbroken after his marriage ends, Theodore (Joaquin Phoenix) becomes fascinated with a new operating system which reportedly develops into an intuitive and unique entity in its own right."];
+    "genre": "learning",
+    "title": "Chefs Table",
+    "picture": "https://miro.medium.com/max/1194/1*pOA5jG_TttmwmGG5gZ9E2A.png",
+    "description": "Some of the most renowned chefs in the world share their deeply personal stories, inspirations, and unique styles. Each chef's discipline and culinary talent is explored while he or she prepares an awe-inspiring creation.",
+    "link": "https://www.netflix.com/search?q=chefs%20table&jbv=80007945"
   },
+
   {
-    "title": "Jenny Slate: Stage Fright",
-    "description": ["Jenny Slate's debut stand up-comedy special blends with home videos from her childhood and interviews of her with her family."];
+    "genre": "sad",
+    "title": "Eternal Sunshine of the Spotless Mind ",
+    "picture": "https://blackgirlnerds.com/wp-content/uploads/2019/04/Screen-Shot-2019-04-19-at-1.23.33-PM.png",
+    "description": "What's messier than a break up? Erasing memories of your ex followed by the surreal discovery of what you've lost.",
+    "link": "https://www.netflix.com/search?q=eternal%20sun&jbv=60034545"
   },
-  {
-    "title": "Athlete A",
-    "description": ["Reporters from The Indianapolis Star expose Dr. Larry Nassar's sexual abuse of young gymnasts."];
-  },
-  {
-    "title": "Taxi Driver",
-    "description": ["When NYC cabbie Travis meets pretty campaign worker Betsy, he becomes obsessed with the idea of saving the world, first plotting to assassinate a presidential candidate, then directing his attentions toward rescuing 12-year-old prostitute Iris."];
-  },
-  {
-    "title": "Spotlight",
-    "description": ["In 2001, editor Marty Baron of The Boston Globe assigns a team of journalists to investigate allegations against John Geoghan, an unfrocked priest accused of molesting more than 80 boys. The reporters make it their mission to provide proof of a cover-up of sexual abuse within the Roman Catholic Church."];
-  },
-  {
-    "title": "I Lost My Body",
-    "description": ["A cut-off hand escapes from a dissection lab with one crucial goal: to get back to its body. As it scrambles through the pitfalls of Paris, it remembers its life with the young man it was once attached to, until they met Gabrielle."];
-  },
+
 ]
 
-for (var i = 0; i < jsonDatabase.length; i++) {
-  createElement(jsonDatabase[i]);
-}
-
 function createElement(incomingJSON) {
+  for (let i = 0; i < incomingJSON.length; i++) {
 
+    let newContentElement = document.createElement("DIV");
+    newContentElement.style.backgroundcolor = "#eee";
+    newContentElement.classList.add('contentItem');
+    newContentElement.classList.add('linkButton');
+    newLink.addEventListener("click", function() {
+      window.location = incomingJSON[i]['link'];
+    });
+    newContentElement.appendChild(newLink);
 
-  let newContentElement = document.createElement("DIV");
-  newContentElement.classList.add('contentItem');
+    let newContentImg = document.createElement("IMG");
+    newContentImg.classList.add("image");
+    newContentImg.src = incomingJSON[i]['picture'];
+    newContentElement.appendChild(newContentImg);
 
-  let newContentHeading = document.createElement("H3");
-  newContentHeading.classList.add('contentTitle');
-  newContentHeading.innerText = incomingJSON['title'];
-  newContentElement.appendChild(newContentHeading);
+    let newContentTitle = document.createElement("H4");
+    newContentTitle.classList.add('contentTitle');
+    newContentTitle.innerText = incomingJSON[i]['title'];
+    newContentElement.appendChild(newContentTitle);
 
-  let newContentSubhead = document.createElement("H4");
-  newContentElement.appendChild(newContentSubhead);
+    let newContentDesc = document.createElement("P");
+    newContentDesc.classList.add('contentDesc');
+    newContentDesc.innerText = incomingJSON[i]['description'];
+    newContentElement.appendChild(newContentDesc);
 
-  let newContentDescription = document.createElement("UL");
-  newContentElement.appendChild(newContentDescription);
+  }
 
-  let newImage = document.createElement("IMG");
-  newImage.classList.add("footerImage");
-  newImage.src = incomingJSON['picture_url'];
-  newContentElement.appendChild(newImage);
-  contentGridElement.appendChild(newContentElement);
+  createElement(jsonDatabase);
 
 
 }
